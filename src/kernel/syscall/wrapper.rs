5a68228f54a94090f1badde5fa8c94ece0479d87
Moritz Riefer <moritz.riefer@freenet.de>
2025-06-02 11:02:11 +0200

    UPDATE: wrapper functions work without Strings

--- src/kernel/syscall/wrapper.rs
+++ src/kernel/syscall/wrapper.rs
@@ -1,35 +1,20 @@
 use crate::kernel::syscall::user_api::usr_read_process_name;
 use crate::kernel::syscall::user_api::usr_get_app_matching_name;
 
-use alloc::string::String;
+pub fn get_process_name(buffer: &mut [u8]) -> &str {
+    let len = usr_read_process_name(buffer.as_mut_ptr(), buffer.len());
 
-pub fn get_process_name() -> String {
-    let mut app_name_buff: [u8; 64] = [0; 64];
-    let ptr = app_name_buff.as_mut_ptr();
-    let name_len = usr_read_process_name(ptr, 64);
-
-    let mut string = String::new();
-    for i in 0..name_len as usize {        
-        let byte = app_name_buff[i];
-        string.push(byte as char);
-    }
-    
-    string
+    match core::str::from_utf8(&buffer[..len as usize]) {
+        Ok(s) => s,
+        Err(_) => "<invalid utf8>",
+    }   
 }
 
-pub fn get_app_matching_name(search_name: String) -> String {
-    let search_name_ptr = search_name.as_ptr();
-    let search_name_len = search_name.len();
+pub fn get_app_matching_name<'a>(buffer: &'a mut [u8], search_name: &[u8], search_name_len: usize) -> &'a str {    
+    let name_len = usr_get_app_matching_name(search_name.as_ptr(), search_name_len, buffer.as_mut_ptr(), 64);
 
-    let mut app_name_buff: [u8; 64] = [0; 64];
-    let ptr = app_name_buff.as_mut_ptr();
-    let name_len = usr_get_app_matching_name(search_name_ptr, search_name_len, ptr, 64);
-
-    let mut string = String::new();
-    for i in 0..name_len as usize {        
-        let byte = app_name_buff[i];
-        string.push(byte as char);
-    }
-    
-    string
+    match core::str::from_utf8(&buffer[..name_len as usize]) {
+        Ok(s) => s,
+        Err(_) => "<invalid utf8>",
+    } 
 }
