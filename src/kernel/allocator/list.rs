cfb018ac2045ba1c7c4ec6b878fe8f65039fdfdb
StellarByteStudios <krollmann.carsten@gmail.com>
2025-06-18 11:03:46 +0200

    [FIX] Allocator setzt jetzt beim Init erst die bounds
    - Allocator wird jetzt komplett vom Kernen ignoriert: Möglichkeit Kprint zu verwenden

--- src/kernel/allocator/list.rs
+++ src/kernel/allocator/list.rs
@@ -42,10 +42,11 @@ impl LinkedListAllocator {
     // This function is unsafe because the caller must guarantee that
     // the given heap bounds are valid. This method must be called only once.
     pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
-        self.add_free_block(heap_start, heap_size);
-
+        self.head = ListNode::new(0);
         self.heap_start = heap_start;
         self.heap_end = heap_start + heap_size - 1;
+
+        self.add_free_block(heap_start, heap_size);
     }
 
     // Adds the given free memory block 'addr' to the front of the free list.
@@ -54,6 +55,21 @@ impl LinkedListAllocator {
         // ensure that the freed block is capable of holding ListNode
         assert_eq!(align_up(addr, mem::align_of::<ListNode>()), addr);
         assert!(size >= mem::size_of::<ListNode>());
+
+        if addr < self.heap_start || addr + size > self.heap_end + 1 {
+            kprint!("===! Fehlerhafter free im Usrlib Allocator! - ");
+            kprintln!(
+                "Daten des Freed Block von alloc [{:#x}  /  {:#x}]\n Heap-Anfang {:#x}, Heap-Size: {:#x}, Heap-Ende: {:#x}",
+                self.heap_start,
+                self.heap_end,
+                addr,
+                size,
+                addr + size
+            );
+            return;
+        }
+
+
         // create a new ListNode (on stack)
         let mut node = ListNode::new(size);
 
--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -1,3 +1,4 @@
+#[cfg(feature = "global-alloc")] // Defaultfeature für Kernel deaktiviert -> allocator Dopplung
 pub mod allocator;
 
 #[cfg(feature = "kprint")] // Defaultfeature für Kernel deaktiviert -> Dopplung im Kernel
